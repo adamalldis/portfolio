9f0b192e27407fe0d2c30e836875ccdf4a5f9d0b
Alldis <98696940+adamalldis@users.noreply.github.com>
2022-03-22 09:05:50 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-const firstVar = "";
+const aboutEl = document.getElementById("about-me");
+const navAboutEl = document.getElementById("nav-about-me");
+
+navAboutEl.addEventListener("click", smoothScroll);
+
+function smoothScroll() {
+    aboutEl.scrollIntoView({behavior: "smooth"});
+}
