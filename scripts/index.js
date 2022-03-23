ef579d55056df0689eb6677a6cc4cb73df386037
Alldis <98696940+adamalldis@users.noreply.github.com>
2022-03-23 09:27:21 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,29 @@
+// ABOUT ME navigation
 const aboutEl = document.getElementById("about-me");
 const navAboutEl = document.getElementById("nav-about-me");
 
-navAboutEl.addEventListener("click", smoothScroll);
+navAboutEl.addEventListener("click", aboutScroll);
 
-function smoothScroll() {
+function aboutScroll() {
     aboutEl.scrollIntoView({behavior: "smooth"});
+}
+
+// SKILLS navigation
+const skillsEl = document.getElementById("skills");
+const navSkillsEl = document.getElementById("nav-skills");
+
+navSkillsEl.addEventListener("click", skillsScroll);
+
+function skillsScroll() {
+    skillsEl.scrollIntoView({behavior: "smooth"});
+}
+
+// PROJECTS navigation
+const projectsEl = document.getElementById("projects");
+const navProjectsEl = document.getElementById("nav-projects");
+
+navProjectsEl.addEventListener("click", projectsScroll);
+
+function projectsScroll() {
+    projectsEl.scrollIntoView({behavior: "smooth"});
 }
