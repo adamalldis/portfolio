8f14e1ce457ad0e56b3bc938c21a7ab682abab21
Alldis <98696940+adamalldis@users.noreply.github.com>
2022-03-25 10:46:16 -0400

    added social links

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
+// HOME return
+const homeEl = document.getElementById("home");
+const navHomeEl = document.getElementById("nav-home");
+
+navHomeEl.addEventListener("click", homeScroll);
+
+function homeScroll() {
+    homeEl.scrollIntoView({behavior: "smooth"});
+}
+
 // ABOUT ME navigation
 const aboutEl = document.getElementById("about-me");
 const navAboutEl = document.getElementById("nav-about-me");
